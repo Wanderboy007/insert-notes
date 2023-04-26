c15a797efb2c40c695c05eba7c0832f6d8d83c78
Wanderboy007 <ctejas44@gmail.com>
2023-04-26 23:06:23 +0530

    front-end changes

---
+++ pages/api/isteacher.js
@@ -0,0 +1,24 @@
+// import initDB from "../../helper/initDB";
+// import user from "../../models/user";
+
+// initDB();
+
+// export default async function getUser (req, res) {
+//   switch (req.method) {
+//     case "GET":
+//       getUser(req, res);
+
+//       break;
+//     case "POST":
+
+//       break;
+//   }
+
+// async function getUser(req, res) {
+//   try {
+//     const resuser = await user.find();
+//     res.status(200).json(resuser);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
